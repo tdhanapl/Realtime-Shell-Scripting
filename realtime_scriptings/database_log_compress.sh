0f6239b232633b34ff0c4be548e7e8bbadc9dc07
dhanapal <dhanapal703278@gmail.com>
2023-01-26 09:59:49 +0530

    new chanages

--- realtime_scriptings/database_log_compress.sh
+++ realtime_scriptings/database_log_compress.sh
@@ -6,9 +6,7 @@
  #date --date="yesterday"
  #date --date="yesterday" | awk '{print $1}'
 #!/bin/bash
-tput setf 2
 day=$(date +"%a")
-touch postgres-$day.log
 previousday=$(date --date="yesterday" | awk '{print $1}')
 day=$(date +"%a")
 rm -rf postgres-$previousday.log.xz
